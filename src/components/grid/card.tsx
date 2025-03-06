162e8987e83b7b2c70d2faa07b4c6c2337be9263
laminsugu <sugufaramuhamed@gmail.com>
2025-03-06 17:01:53 +0000

    cambio titolo 1

--- src/components/grid/card.tsx
+++ src/components/grid/card.tsx
@@ -5,7 +5,7 @@ interface props {
     name:string;
     types: string[];
 }
-export const card:React.FC<props> = (props){
+export const card:React.FC<props> = (props)=>{
    return <div className="w-2xs p-8">{props.name}
    <img src={props.image}/>
    </div>
