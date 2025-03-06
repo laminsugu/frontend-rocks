9a8c0da4cd70c9875ecfafe8e032b62ba291cedd
laminsugu <sugufaramuhamed@gmail.com>
2025-03-06 11:03:59 +0000

    Change title

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import viteLogo from "/vite.svg";
 
 export const App = () => {
   const [count, setCount] = useState(0);
-
+const[title,settitle]=useState("Green ninja ash");
   return (
     <div className="h-dvh flex flex-col items-center justify-center">
       <div className="bg-white p-8 rounded-md shadow-lg">
         <h1 className="text-center font-bold text-3xl text-blue-400 mb-4">
-          Frontend Rocks
+         {title}
+        
         </h1>
 
         <div className="flex justify-center space-x-4 mb-2">
@@ -25,17 +26,23 @@ export const App = () => {
 
         <div className="flex flex-col items-center space-y-4">
           <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
+            className="bg-green-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
             onClick={() => setCount((count) => count + 1)}
           >
             Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
+          </button>
+          <button
+          className="bg-green-500 text white px-4 py-2 runded-md shadow-md cursor pointer"
+          onClick={() => settitle("evee")}
+          > 
+          cambia titolo
           </button>
           <p className="text-center">
             Modifica <code>src/App.tsx</code> e salva per testare l'hot reload
           </p>
         </div>
 
-        <p className="text-center text-sm text-blue-400">Clicca sui loghi per saperne di più</p>
+        <p className="text-center text-sm text-green-400">Clicca sui loghi per saperne di più</p>
       </div>
     </div>
   );
